4961a48998a0bf72b2c4b76ba015ed80245893d4
gstaaij <gstaaij@protonmail.com>
2024-05-30 16:47:55 +0200

    Add zoom and pan controls
    Also moved the FPS counter out of the way and adjusted the
    current player indicators to always have a background color

--- src/tictactoes.c
+++ src/tictactoes.c
@@ -12,6 +12,10 @@
 #define ARENA_IMPLEMENTATION
 #include "arena/arena.h"
 
+#define SCROLL_ZOOM_SPEED 7.5f
+#define KEY_ZOOM_SPEED 1.5f
+#define KEY_MOVE_SPEED 750.0f
+
 static Arena arena = {0};
 
 int main() {
@@ -23,21 +27,33 @@ int main() {
     // Initialize the game
     Game game = gridCreate(&arena, 2);
     GameState state = GAME_STATE_CROSS;
+    Vector2 cameraPos = {0};
+    float scaleExp = 0.0f;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(BACKGROUND_COLOR);
-            DrawFPS(10, 10);
 
             float screenWidth = GetScreenWidth();
             float screenHeight = GetScreenHeight();
             float size = fminf(screenWidth, screenHeight);
 
-            Rectangle rect = { screenWidth/2 - size/2, screenHeight/2 - size/2, size, size };
+            Rectangle rect = { -size/2, -size/2, size, size };
 
-            game.draw(game.data, rect, FOREGROUND_COLOR);
+            Camera2D camera = {
+                .offset = { screenWidth/2, screenHeight/2 },
+                .target = cameraPos,
+                .zoom = powf(2, scaleExp),
+                .rotation = 0.0f,
+            };
 
-            float offsetFromBorder = 10.0f;
+            BeginMode2D(camera);
+                game.draw(game.data, rect, FOREGROUND_COLOR);
+            EndMode2D();
+
+            Vector2 offset = Vector2Transform(Vector2Zero(), GetCameraMatrix2D(camera));
+
+            float offsetFromBorder = 5.0f;
             float currentPlayerSize = size / 10;
             if (currentPlayerSize > fabsf(screenWidth - screenHeight)) {
                 currentPlayerSize = GRID_PADDING * size - offsetFromBorder;
@@ -45,13 +61,21 @@ int main() {
             float x = offsetFromBorder;
             float yw = screenWidth - offsetFromBorder - currentPlayerSize;
             float yh = screenHeight - offsetFromBorder - currentPlayerSize;
+            // Top left
+            DrawRectangleV(      (Vector2) { x,  x  }, (Vector2) {currentPlayerSize, currentPlayerSize}, BACKGROUND_COLOR);
             cellDrawState(state, (Vector2) { x,  x  }, currentPlayerSize, FOREGROUND_COLOR);
+            // Bottom left
+            DrawRectangleV(      (Vector2) { x,  yh }, (Vector2) {currentPlayerSize, currentPlayerSize}, BACKGROUND_COLOR);
             cellDrawState(state, (Vector2) { x,  yh }, currentPlayerSize, FOREGROUND_COLOR);
+            // Top right
+            DrawRectangleV(      (Vector2) { yw, x  }, (Vector2) {currentPlayerSize, currentPlayerSize}, BACKGROUND_COLOR);
             cellDrawState(state, (Vector2) { yw, x  }, currentPlayerSize, FOREGROUND_COLOR);
+            // Bottom right
+            DrawRectangleV(      (Vector2) { yw, yh }, (Vector2) {currentPlayerSize, currentPlayerSize}, BACKGROUND_COLOR);
             cellDrawState(state, (Vector2) { yw, yh }, currentPlayerSize, FOREGROUND_COLOR);
 
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                Vector2 mousePos = GetMousePosition();
+                Vector2 mousePos = Vector2Scale(Vector2Subtract(GetMousePosition(), offset), 1 / camera.zoom);
 
                 if (mousePos.x >= rect.x && mousePos.x < rect.x + rect.width &&
                     mousePos.y >= rect.y && mousePos.y < rect.y + rect.height &&
@@ -61,12 +85,34 @@ int main() {
                 }
             }
 
+            if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                Vector2 mouseMove = GetMouseDelta();
+                cameraPos = Vector2Subtract(cameraPos, Vector2Scale(mouseMove, 1.0f / camera.zoom));
+            }
+
+            scaleExp += GetMouseWheelMove() * SCROLL_ZOOM_SPEED * GetFrameTime();
+
+            if (IsKeyDown(KEY_MINUS)) {
+                scaleExp -= KEY_ZOOM_SPEED * GetFrameTime();
+            }
+            if (IsKeyDown(KEY_EQUAL)) {
+                scaleExp += KEY_ZOOM_SPEED * GetFrameTime();
+            }
+
+            int leftright = IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT);
+            int updown = IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP);
+            cameraPos.x += leftright / camera.zoom * KEY_MOVE_SPEED * GetFrameTime();
+            cameraPos.y += updown / camera.zoom * KEY_MOVE_SPEED * GetFrameTime();
+
             if (IsKeyPressed(KEY_R)) {
                 arena_reset(&arena);
                 game = gridCreate(&arena, 2);
                 state = GAME_STATE_CROSS;
+                cameraPos = (Vector2){0};
+                scaleExp = 0.0f;
             }
 
+            DrawFPS(10, currentPlayerSize + 10);
         EndDrawing();
     }
 
